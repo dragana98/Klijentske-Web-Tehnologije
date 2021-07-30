63bf8acf86de40c321d69a8b2e7da2f9b9088388
Dragana Andjelkovic <dragana.andjelkovic98@gmail.com>
2021-07-30 22:13:02 +0200

    Update datumjs.js

--- javascripts/datumjs.js
+++ javascripts/datumjs.js
@@ -1,7 +1,12 @@
-function danasnjiDatum()
-{
-    
-var today=new Date();
 
-document.getElementById("datum").innerHTML = today.toLocaleDateString();
-}
+var today = new Date();
+    
+var month = today.getMonth() + 1;
+var year = today.getFullYear();
+var day = today.getDate();
+
+var current = day+"."+month+"."+year+".";
+
+document.getElementById("datum").innerHTML = current;
+document.getElementById("datum").style.fontSize = "16px";
+
